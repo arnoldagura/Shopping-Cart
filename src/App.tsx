69a13548038aa62943413965d0738c0bfbc58986
Arnold Agura <arnold.agura@brank.as>
2023-02-17 02:38:53 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,9 @@ import About from "./pages/About"
 import Home from "./pages/Home"
 import Store from "./pages/Store"
 import NavBar from "./components/NavBar"
-import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 function App() {
   return (
-    <ShoppingCartProvider>
+    <>
       <NavBar />
       <Container className="mb-4">
         <Routes>
@@ -16,7 +15,7 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </Container>
-    </ShoppingCartProvider>
+    </>
   )
 }
 
